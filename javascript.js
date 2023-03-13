6320d36288d859d1205c9dbef07f88da84d28981
PAnkita123 <malviyaankita901@gmail.com>
2023-03-13 19:53:24 +0530

    commit

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ function sign_in(event) {
     console.log(dataFromLS, "dataFromLS")
 
     var flag = false;
-    for (i = 0; i < dataFromLS.length; i++) {
+    for ( var i = 0; i < dataFromLS.length; i++) {
         if (dataFromLS[i].email === userEmail) {
             flag = true;
         }
@@ -57,11 +57,52 @@ function login(event) {
         document.getElementById("Email").value = "";
         document.getElementById("Password").value = "";
         alert("You are logged in successfully!");
+        var myntraUser={};
+        myntraUser["current-user-email"]=userEmail;
+        localStorage.setItem("myntraUser", JSON.stringify( myntraUser))
         window.location.href = "./index.html";
 
     } else {
         alert("Please enter correct credentials!")
     }
 
+
+}
+
+var gettingEmail;
+function forgetPassword(){
+    var dataFromLS=JSON.parse(localStorage.getItem("userData2"));
+    var userEmail=document.getElementById("email").value;
+    gettingEmail=userEmail;
+    var flag = false;
+    for (var i = 0; i <dataFromLS.length; i++) {
+        if (dataFromLS[i].email===userEmail)
+            flag = true;
+    }
+
+    if (flag === true) {
+        var newCode = `<div id="set-pass"><small>Set a New Password!</small><br/><input type="password" id="password"/><br/><br/><button onclick="newPassword()">set new password</button></div>`
+        var divFromHTML=document.getElementById("change") 
+        divFromHTML.innerHTML=newCode;
+        alert('set new Password!')
+
+    }else{
+        alert("please check your email, its not in our database!")
+    }
+}
+
+function newPassword(){
+    var userPassword=document.getElementById("password").value;
+    var dataFromLS=JSON.parse(localStorage.getItem("userData2"));
+
+    for(var i=0; i<dataFromLS.length;i++){
+        if(dataFromLS[i].email === gettingEmail){
+            dataFromLS[i].password = userPassword
+        } 
+    }
+    localStorage.setItem('userData2', JSON.stringify(dataFromLS));
+    gettingEmail="";
+    window.location.href='./login.html';
+    alert("password changed, now login.")
 
 }
